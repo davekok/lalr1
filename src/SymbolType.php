0a3aee49fad71109de4f76a94b52390895d46088
Gitea <gitea@fake.local>
2021-10-17 13:17:51 +0200

    work in progress

--- src/SymbolType.php
+++ src/SymbolType.php
@@ -9,13 +9,4 @@ enum SymbolType: string
     case ROOT = "root";
     case BRANCH = "branch";
     case LEAF = "leaf";
-
-    public function label(): string
-    {
-        return match($this) {
-            self::ROOT => "root",
-            self::BRANCH => "branch",
-            self::LEAF => "leaf",
-        };
-    }
 }
--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace davekok\lalr1\tests;
 
-use davekok\lalr1\Key;
 use davekok\lalr1\Rule;
 use davekok\lalr1\Rules;
 use davekok\lalr1\RulesFactory;
 use davekok\lalr1\Symbol;
-use davekok\lalr1\Symbols;
 use davekok\lalr1\SymbolType;
 use davekok\lalr1\Token;
 use PHPUnit\Framework\TestCase;
@@ -18,27 +16,28 @@ use ReflectionClass;
 class RulesTest extends TestCase
 {
     /**
-     * @covers \davekok\lalr1\Rules::__construct
-     * @covers \davekok\lalr1\Rules::getSymbol
-     * @covers \davekok\lalr1\Rules::getRule
-     * @covers \davekok\lalr1\Rules::solution
      * @covers \davekok\lalr1\Rule::__construct
-     * @covers \davekok\lalr1\Rule::setKey
-     * @covers \davekok\lalr1\Rule::setPrecedence
-     * @covers \davekok\lalr1\Rule::setReduceMethod
+     * @covers \davekok\lalr1\Rule::__toString
      * @covers \davekok\lalr1\Rule::reduce
-     * @covers \davekok\lalr1\Token::__construct
+     * @covers \davekok\lalr1\Rules::__construct
+     * @covers \davekok\lalr1\Rules::getRule
+     * @covers \davekok\lalr1\Rules::getSymbol
+     * @covers \davekok\lalr1\Rules::solution
      * @uses \davekok\lalr1\Symbol
-     * @uses \davekok\lalr1\Symbols
-     * @uses \davekok\lalr1\Key
+     * @uses \davekok\lalr1\Token
      */
     public function testRules(): void
     {
-        $value        = new Symbol(SymbolType::ROOT, "value");
-        $object       = new Symbol(SymbolType::BRANCH, "object");
-        $openingBrace = new Symbol(SymbolType::LEAF, "opening-brace");
-        $closingBrace = new Symbol(SymbolType::LEAF, "closing-brace");
-        $symbols      = new Symbols($value, $object, $openingBrace, $closingBrace);
+        $value        = new Symbol(SymbolType::ROOT, "\0", "value", 0);
+        $object       = new Symbol(SymbolType::BRANCH, "\1", "object", 0);
+        $openingBrace = new Symbol(SymbolType::LEAF, "\2", "opening-brace", 0);
+        $closingBrace = new Symbol(SymbolType::LEAF, "\3", "closing-brace", 0);
+        $symbols      = [
+            "value"         => $value,
+            "object"        => $object,
+            "opening-brace" => $openingBrace,
+            "closing-brace" => $closingBrace,
+        ];
         $key1         = $object->key;
         $key2         = $openingBrace->key . $closingBrace->key;
         $rulesObject  = new class() {
@@ -49,20 +48,17 @@ class RulesTest extends TestCase
             }
         };
         $reflection = new ReflectionClass($rulesObject);
-        $expected1  = new Rule("object", 0);
-        $expected1->setKey($key1);
-        $expected1->setReduceMethod($reflection->getMethod("rule1"));
-        $expected2 = new Rule("opening-brace closing-brace");
-        $expected2->setKey($key2);
-        $expected2->setPrecedence(0);
-        $expected2->setReduceMethod($reflection->getMethod("rule2"));
+        $expected1  = new Rule($key1, "object", 0, $reflection->getMethod("rule1"));
+        static::assertSame('[0] object', (string)$expected1);
+        $expected2 = new Rule($key2, "opening-brace closing-brace", 0, $reflection->getMethod("rule2"));
+        static::assertSame('[0] opening-brace closing-brace', (string)$expected2);
         $rules = new Rules($symbols, [$key1 => $expected1, $key2 => $expected2], $reflection->getMethod("solution"));
         static::assertSame($value, $rules->getSymbol("value"));
         static::assertSame($object, $rules->getSymbol("object"));
         static::assertSame($openingBrace, $rules->getSymbol("opening-brace"));
         static::assertSame($closingBrace, $rules->getSymbol("closing-brace"));
         $token = new Token($value, 1);
-        static::assertSame("root:00:value:0:1", "$token");
+        static::assertSame('{"symbol":{"type":"root","key":"\u0000","name":"value","precedence":0},"value":1}', "$token");
         static::assertSame($token, $rules->getRule($key1)->reduce($rulesObject, [$token]));
         static::assertSame($token, $rules->getRule($key2)->reduce($rulesObject, [$token]));
         $rules->getSymbol("closing-brace");
